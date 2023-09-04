e0caf9738d393ef80ba58b8b79ef8459b26b7dc0
GonzaloLlatser <gonllat@gmail.com>
2023-09-04 15:04:25 +0200

    style: Crea y da estilos generales a la pagina inicial.

--- Blog-Mern-Completo/src/components/pages/Inicio.jsx
+++ Blog-Mern-Completo/src/components/pages/Inicio.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div className='jumbo'>
+      <h1>Bienvenido al blog con React</h1>
+      <p>Blog desarrollado con el MERN Stack (Mongo, Express, React y NodeJS)</p>
+      <Link to = '/Articulos' className='button'>Ver los artículos</Link>
+    </div>
   )
 }
