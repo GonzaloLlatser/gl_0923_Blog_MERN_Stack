7b176d8354ac4a974e77f5d0a26d3be9e15f15e7
GonzaloLlatser <gonllat@gmail.com>
2023-09-04 11:20:55 +0200

    feat: Genera Script para facilitar el inicio de la ejecucion del proyecto, se observa funcionamiento correcto.

---
+++ arrancar-proyecto.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe"
+start cmd.exe /k "cd C:\Users\gonll\Desktop\PROGRAMACION\REACT UDEMY VICTOR ROBLES\RETOS Y PROYECTOS\api-rest-node && npm start"
+start cmd.exe /k "cd C:\Users\gonll\Desktop\PROGRAMACION\REACT UDEMY VICTOR ROBLES\RETOS Y PROYECTOS\gl_0923_react_mern_blog\Blog-Mern-Completo && npm run dev"
+start C:\Users\gonll\AppData\Local\MongoDBCompass\MongoDBCompass.exe
+start C:\Users\gonll\AppData\Local\Postman\Postman.exe
