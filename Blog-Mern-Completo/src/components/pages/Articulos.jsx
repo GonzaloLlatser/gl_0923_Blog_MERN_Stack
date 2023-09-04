c8133791c1abfa2f1e194f7e4352cc1ce1770ff5
GonzaloLlatser <gonllat@gmail.com>
2023-09-04 18:20:53 +0200

    style: Aplca estilos y primeros parametros visuales a la pagina de articulos.

--- Blog-Mern-Completo/src/components/pages/Articulos.jsx
+++ Blog-Mern-Completo/src/components/pages/Articulos.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+{/* Aqui van los articulos */ }
+
 export const Articulos = () => {
   return (
-    <div>Articulos</div>
+    <>
+      <article className="articulo-item">
+        <div className='mascara'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" />
+        </div>
+        <div className='datos'>
+          <h3 className="title">Desarrollo Web</h3>
+          <p className="description">articulo</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </div>
+      </article>
+    </>
   )
 }
